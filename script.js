bcf2e51ad3f141037c71d8876f730d040d1008c9
hastiafra <77177165+hastiafra@users.noreply.github.com>
2024-03-26 21:32:57 -0400

    update

--- script.js
+++ script.js
@@ -4,26 +4,24 @@ import { APIKEY } from "./env.js";
 let startBtn = document.getElementById("startBtn");
 const audioElement = document.getElementById("audio");
 
-const audioControl =() =>{
+const audioControl = () => {
   startBtn.disabled = !startBtn.disabled;
-}
+};
 
-
-const tellJoke = (joke)=>{
+const tellJoke = (joke) => {
   VoiceRSS.speech({
-        key: `${APIKEY}`,
-        src: joke,
-        hl: "en-us",
-        v: "Linda",
-        r: 0,
-        c: "mp3",
-        f: "44khz_16bit_stereo",
-        ssml: false,
-      });
-}
+    key: `${APIKEY}`,
+    src: joke,
+    hl: "en-us",
+    v: "Linda",
+    r: 0,
+    c: "mp3",
+    f: "44khz_16bit_stereo",
+    ssml: false,
+  });
+};
 
-
-//fetching joke 
+//fetching joke
 const getJokes = async () => {
   let joke = "";
   try {
@@ -32,20 +30,17 @@ const getJokes = async () => {
     );
 
     const data = await res.json();
-    if(data.setup){
-      joke = `${data.setup}...${data.delivery}`
-    } else{
-      joke = `${data.joke}`
+    if (data.setup) {
+      joke = `${data.setup}...${data.delivery}`;
+    } else {
+      joke = `${data.joke}`;
     }
     tellJoke(joke);
     audioControl();
-  
   } catch (err) {
     console.log("jokesAPI", err);
   }
 };
 
-
-
 startBtn.addEventListener("click", getJokes);
-audioElement.addEventListener("ended", audioControl)
+audioElement.addEventListener("ended", audioControl);
